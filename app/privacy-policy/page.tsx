aeaa018a6f76f4a367c09526469d6668af3e6d3b
Taras Pakhaliuk <info@exzellentia-constructio.de>
2026-08-02 23:47:37 +0200

    Fix privacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPolicyPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-white">
+      <h1>Privacy Policy</h1>
+    </main>
+  );
+}
